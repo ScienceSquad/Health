7d1b6e19df56d4dddf54996752913eed48f18cb8
Andrew Walden <waldena@purdue.edu>
2016-01-31 15:06:38 -0500

    Fixed build issue

--- Health/src/main/java/com/sciencesquad/health/health/MainActivity.java
+++ Health/src/main/java/com/sciencesquad/health/health/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_health_main);
+		setContentView(R.layout.activity_main);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
