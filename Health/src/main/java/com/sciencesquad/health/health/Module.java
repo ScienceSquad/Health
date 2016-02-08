b7e12aacaf9d21ca0dcc11d81655ca87ec471cca
Aditya Vaidyam <avaidyam@purdue.edu>
2016-02-08 00:33:26 -0500

    Cleaned up comments and added a ForkJoinPool.

--- Health/src/main/java/com/sciencesquad/health/health/Module.java
+++ Health/src/main/java/com/sciencesquad/health/health/Module.java
@@ -5,18 +5,31 @@ import rx.Subscription;
 import rx.functions.Action1;
 
 import java.util.HashSet;
+import java.util.concurrent.ForkJoinPool;
 
 public abstract class Module {
 
 	private static final String TAG = "Module";
 
+	private static int PROCESSORS = Runtime.getRuntime().availableProcessors() * 2;
+	private static ForkJoinPool POOL = new ForkJoinPool(PROCESSORS);
+
 	private HashSet<Subscription> _subscriptions = new HashSet<>();
 
 	/**
-	 * Helper to publish any events needed.
+	 * Returns the concurrent execution service for this Module.
 	 *
-	 * @param event
-	 * @param <E>
+	 * @return a ForkJoinPool shared by all Modules.
+	 */
+	protected ForkJoinPool executor() {
+		return Module.POOL;
+	}
+
+	/**
+	 * Publishes any Events to the shared application EventBus.
+	 *
+	 * @param event the event to publish
+	 * @param <E> the type of Event being published
 	 */
 	public <E extends EventBus.Event> void publish(E event) {
 		BridgeApplication.application().ifPresent(app -> {
@@ -25,12 +38,14 @@ public abstract class Module {
 	}
 
 	/**
-	 * Helper to subscribe and auto-manage any Subscriptions.
-	 * Relies on the invocation of finalize() to clean up.
+	 * Subscribes and auto-manage a Subscription to an Event.
+	 * Automatically uses the shared application EventBus.
 	 *
-	 * @param eventClass
-	 * @param handler
-	 * @param <E>
+	 * @implNote Relies on the invocation of finalize() to clean up.
+	 *
+	 * @param eventClass the type of Event subscribed to
+	 * @param handler the action to perform upon notification
+	 * @param <E> the type of Event being subscribed to
 	 */
 	public <E extends EventBus.Event> void subscribe(final Class<E> eventClass, Action1<E> handler) {
 		BridgeApplication.application().ifPresent(app -> {
@@ -39,12 +54,17 @@ public abstract class Module {
 		});
 	}
 
+	/**
+	 * Invoked upon garbage collection or deallocation of the Module.
+	 * If the current Runtime does not invoke this method automatically,
+	 * it MUST be invoked manually, to ensure Subscriptions are dealt with.
+	 *
+	 * @throws Throwable a generic error
+	 */
 	@Override
 	protected void finalize() throws Throwable {
 		super.finalize();
 		StreamSupport.stream(this._subscriptions).forEach(Subscription::unsubscribe);
 		this._subscriptions.clear();
 	}
-
-	// things go here.
 }
