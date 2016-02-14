c1504f784b11c03c84310af707eebcd87d7ddf97
Aditya Vaidyam <avaidyam@purdue.edu>
2016-02-13 20:28:19 -0500

    Initial SensorContext implementation. Not sure if anything really works yet.

---
+++ Health/src/main/java/com/sciencesquad/health/events/SensorContext.java
@@ -0,0 +1,129 @@
+package com.sciencesquad.health.events;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Looper;
+import com.sciencesquad.health.BaseApplication;
+import java8.util.Optional;
+import org.immutables.value.Value.Immutable;
+
+import java.util.HashMap;
+
+import static com.sciencesquad.health.events.Event.EventType;
+
+/**
+ *
+ */
+public class SensorContext {
+
+	/**
+	 *
+	 */
+	public class SensorNotAvailableException extends Exception {
+		public SensorNotAvailableException(String message) {
+			super(message);
+		}
+ 	}
+
+	/**
+	 *
+	 */
+	@Immutable @EventType
+	public interface SensorChange extends Event {
+
+		/**
+		 *
+		 * @return
+		 */
+		SensorEvent sensorEvent();
+	}
+
+	/**
+	 *
+	 */
+	@Immutable @EventType
+	public interface SensorAccuracy extends Event {
+
+		/**
+		 *
+		 * @return
+		 */
+		Sensor sensor();
+
+		/**
+		 *
+		 * @return
+		 */
+		int accuracy();
+	}
+
+	private SensorManager _sensorManager;
+	private HashMap<Integer, SensorEventListener> _sensorListeners = new HashMap<>();
+
+	/**
+	 *
+	 * @param context
+	 */
+	public SensorContext(final Context context) {
+		this._sensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+	}
+
+	/**
+	 *
+	 * @param sensorType
+	 * @param samplePeriod
+	 * @throws SensorNotAvailableException
+	 */
+	public void requestSensorEvents(int sensorType, int samplePeriod) throws SensorNotAvailableException {
+		final Sensor sensor = this._sensorManager.getDefaultSensor(sensorType);
+		if (sensor == null || this._sensorListeners.containsKey(sensorType)) {
+			throw new SensorNotAvailableException("Sensor " + sensorType + " is not available on this device!");
+		}
+
+		final SensorEventListener listener = new SensorEventListener() {
+			@Override public void onSensorChanged(SensorEvent sensorEvent) {
+				Optional.ofNullable(BaseApplication.application())
+						.map(BaseApplication::eventBus)
+						.ifPresent(e -> {
+							e.publish(SensorChangeEvent.from(this)
+									.sensorEvent(sensorEvent)
+									.create());
+						});
+			}
+
+			@Override public void onAccuracyChanged(Sensor sensor, int accuracy) {
+				Optional.ofNullable(BaseApplication.application())
+						.map(BaseApplication::eventBus)
+						.ifPresent(e -> {
+							e.publish(SensorAccuracyEvent.from(this)
+									.sensor(sensor)
+									.accuracy(accuracy)
+									.create());
+						});
+			}
+		};
+
+		this._sensorListeners.put(sensorType, listener);
+		this._sensorManager.registerListener(listener, sensor, samplePeriod);
+	}
+
+	/**
+	 *
+	 * @param sensorType
+	 */
+	public void relinquishSensorEvents(int sensorType) {
+		Optional.ofNullable(this._sensorListeners.get(sensorType))
+				.ifPresent(l -> {
+					Runnable action = () -> this._sensorManager.unregisterListener(l);
+
+					// Ensure execution on the main thread if not already there.
+					if (Looper.getMainLooper() != Looper.myLooper())
+						new Handler(Looper.getMainLooper()).post(action);
+					else action.run();
+				});
+	}
+}
