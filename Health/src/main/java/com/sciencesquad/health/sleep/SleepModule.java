45f3eef7e1e3eb02809e5b59cbfdf3bc984b183d
Aditya Vaidyam <avaidyam@purdue.edu>
2016-02-22 10:49:36 -0500

    Create initial SleepModule.

---
+++ Health/src/main/java/com/sciencesquad/health/sleep/SleepModule.java
@@ -0,0 +1,10 @@
+package com.sciencesquad.health.sleep;
+
+import com.sciencesquad.health.Module;
+
+public class SleepModule extends Module {
+
+	public SleepModule() {
+		//this.subscribe(App);
+	}
+}
