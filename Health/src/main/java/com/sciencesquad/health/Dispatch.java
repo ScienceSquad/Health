439375ea37ab7eb7d89ef3b18c02945bc1c256b6
Aditya Vaidyam <avaidyam@purdue.edu>
2016-02-24 12:29:51 -0500

    Implemented Dispatcher -- the multithreading core of the application!

--- Health/src/main/java/com/sciencesquad/health/Dispatch.java
+++ Health/src/main/java/com/sciencesquad/health/Dispatch.java
@@ -1,19 +1,137 @@
 package com.sciencesquad.health;
 
-import java.util.concurrent.ForkJoinPool;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import java8.util.concurrent.CompletableFuture;
 
-public class Dispatch {
-	// multithreading madness goes here
+import java.util.concurrent.Callable;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-	private static int PROCESSORS = Runtime.getRuntime().availableProcessors() * 2;
-	private static ForkJoinPool POOL = new ForkJoinPool(PROCESSORS);
+public enum Dispatcher {
 
 	/**
-	 * Returns the concurrent execution service for this Module.
 	 *
-	 * @return a ForkJoinPool to be used for computationally intensive tasks.
 	 */
-	protected ForkJoinPool executor() {
-		return Dispatch.POOL;
+	MAIN {
+
+	},
+
+	/**
+	 *
+	 */
+	HIGH {
+
+	},
+
+	/**
+	 *
+	 */
+	DEFAULT {
+
+	},
+
+	/**
+	 *
+	 */
+	LOW {
+
+	},
+
+	/**
+	 *
+	 */
+	BACKGROUND {
+
+	};
+
+	/**
+	 * A convenience method to execute a Runnable action on the UI thread.
+	 * This should be used for all UI-related or concurrent interactions.
+	 *
+	 * Using the Future provided, the action may become synchronous or asynchronous
+	 * depending on whether `.then()` or `.wait()` is called. Asynchronous
+	 * is preferred, unless a UI action or something similar requires sequential
+	 * processing.
+	 *
+	 * @param action the action to execute on the Dispatcher
+	 */
+	public CompletableFuture<Void> run(@NonNull Runnable action) {
+		return this.run(action, 0, null);
 	}
+
+	/**
+	 * A convenience method to execute a Runnable action on the UI thread.
+	 * This should be used for all UI-related interactions.
+	 *
+	 * @param action the action to execute on the UI thread
+	 */
+	public <T> CompletableFuture<T> run(@NonNull Callable<T> action) {
+		return this.run(action, 0, null);
+	}
+
+	/**
+	 * A convenience method to execute a Runnable action on the UI thread
+	 * after a duration of time (i.e. delayed action).
+	 * This should be used for all UI-related interactions.
+	 *
+	 * @param action the action to execute on the UI thread
+	 * @param duration the delay time after which the action is executed
+	 * @param timeUnit the time unit corresponding to duration
+	 */
+	public CompletableFuture<Void> run(@NonNull Runnable action, long duration, @Nullable TimeUnit timeUnit) {
+		return this.run(() -> {
+			action.run();
+			return null;
+		}, duration, timeUnit);
+	}
+
+	/**
+	 * A convenience method to execute a Runnable action on the UI thread
+	 * after a duration of time (i.e. delayed action).
+	 * This should be used for all UI-related interactions.
+	 *
+	 * @param action the action to execute on the UI thread
+	 * @param duration the delay time after which the action is executed
+	 * @param timeUnit the time unit corresponding to duration
+	 */
+	public <T> CompletableFuture<T> run(@NonNull Callable<T> action, long duration, @Nullable TimeUnit timeUnit) {
+		final CompletableFuture<T> future = new CompletableFuture<>();
+
+		// If duration and timeUnit aren't present, we're not scheduling the action.
+		long time = (duration == 0 || timeUnit == null) ? 0 : timeUnit.toMillis(duration);
+
+		// Wrap the callable or runnable to interact with the future.
+		Runnable func = () -> {
+			try {
+				future.complete(action.call());
+			} catch (Exception e) {
+				future.completeExceptionally(e);
+			}
+		};
+
+		// Depending on which Dispatch we are, execute the action differently.
+		// TODO: Currently does not support non-MAIN priorities.
+		switch (this) {
+			case MAIN:
+				UITHREAD.postDelayed(func, time);
+				break;
+			case HIGH:
+			case DEFAULT:
+			case LOW:
+			case BACKGROUND:
+				EXEC.schedule(func, time, TimeUnit.MILLISECONDS);
+				break;
+		}
+		return future;
+	}
+
+	// Private Executor for non-MAIN priorities.
+	private static int PROCESSORS = Runtime.getRuntime().availableProcessors() * 2;
+	private static PriorityBlockingQueue<Runnable> QUEUE = new PriorityBlockingQueue<>();
+	private static Handler UITHREAD = new Handler(Looper.getMainLooper());
+	private static ScheduledThreadPoolExecutor EXEC = new ScheduledThreadPoolExecutor(PROCESSORS);
 }
