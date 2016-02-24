bea1a9087046c1c298fcb1bd87bf8ffe87d3bb97
Aditya Vaidyam <avaidyam@purdue.edu>
2016-02-24 17:36:29 -0500

    Commented ViewContext.

--- Health/src/main/java/com/sciencesquad/health/ViewContext.java
+++ Health/src/main/java/com/sciencesquad/health/ViewContext.java
@@ -6,12 +6,33 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 
 /**
+ * Defines the interaction between a Module and its View.
  *
- * @param <T>
+ * @param <T> a generated subclass of ViewDataBinding
  */
-public interface ViewContext<T extends ViewDataBinding>  {
+public interface ViewContext<T extends ViewDataBinding> {
+
+	/**
+	 * The ViewDataBinding that hooks the View and ViewModel.
+	 * @return the ViewDataBinding that hooks the View and the ViewModel
+	 */
 	T binding();
+
+	/**
+	 * The Activity in which the View is displayed.
+	 * @return the Activity in which the View is displayed
+	 */
 	Activity activity();
+
+	/**
+	 * The options bundle for the View.
+	 * @return the options bundle for the View
+	 */
 	Bundle bundle();
+
+	/**
+	 * The layout resource ID.
+	 * @return the layout resource ID
+	 */
 	@LayoutRes int layout();
 }
