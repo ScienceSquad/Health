a4a3bbf6e45026ab2b2bdcec6f7f4e68e571f0fd
Daniel Miller <mill1298@purdue.edu>
2016-02-24 21:43:14 -0500

    Tiny clean up

--- Health/src/main/java/com/sciencesquad/health/data/RealmContext.java
+++ Health/src/main/java/com/sciencesquad/health/data/RealmContext.java
@@ -3,7 +3,6 @@ package com.sciencesquad.health.data;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.view.animation.FastOutLinearInInterpolator;
 
 import com.sciencesquad.health.events.BaseApplication;
 
