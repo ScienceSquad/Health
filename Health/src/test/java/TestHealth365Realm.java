05ac40739b113136d656b83d532322017348a696
Daniel Miller <mill1298@purdue.edu>
2016-02-25 14:06:56 -0500

    Init Unit Test Commit.

---
+++ Health/src/test/java/TestHealth365Realm.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Units tests for Health365
+ */
+public class TestHealth365Realm {
+    @Test
+    public void validdateRealm(){
+
+    }
+}
