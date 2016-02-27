f5e43ef48cb8c50cc5401638fee8f4598e067bda
Colin Ford <colinford@purdue.edu>
2016-02-27 14:42:19 -0800

    some UI tweaks
    Just tooling around. Hopefully everyone is fine with this. Adjusted
    some of the icons with android vector drawables, trying to implement
    basic UI for the steps module

---
+++ Health/src/main/java/com/sciencesquad/health/steps/StepsActivity.java
@@ -0,0 +1,12 @@
+package com.sciencesquad.health.steps;
+
+import com.sciencesquad.health.events.BaseActivity;
+
+/**
+ * This is the only way I know how to do this as of right now. Should this be in my Model instead?
+ * Will write more in a few.
+ */
+public class StepsActivity extends BaseActivity {
+
+
+}
