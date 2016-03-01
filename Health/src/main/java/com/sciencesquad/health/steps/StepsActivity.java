b85aad90fa5875dbf2c4b8b73d6257071238ef45
Colin Ford <colinford@purdue.edu>
2016-02-29 23:21:38 -0800

    fixed silly mistake
    Look away! There is nothing to see here. Besides the UI being a
    nightmare, this should be testable now!

--- Health/src/main/java/com/sciencesquad/health/steps/StepsActivity.java
+++ Health/src/main/java/com/sciencesquad/health/steps/StepsActivity.java
@@ -167,7 +167,7 @@ public class StepsActivity extends BaseActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if present
-        getMenuInflater().inflate((R.menu.health_steps, menu)); // I don't know about this yet
+        getMenuInflater().inflate(R.menu.health_steps, menu); // I don't know about this yet
         return true;
     }
 
@@ -176,7 +176,6 @@ public class StepsActivity extends BaseActivity
         numSteps = 0;
         tvSteps.setText(String.valueOf(numSteps));
     }
-
 
 
     @SuppressWarnings("StatementWithEmptyBody")
