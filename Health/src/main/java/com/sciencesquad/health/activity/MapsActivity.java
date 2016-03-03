581ec7725a3499826c82df075ef9e669b2ced19f
brandonschabell <bschabel@purdue.edu>
2016-03-03 17:22:38 -0500

    Location is working correctly. Map is able to get my exact location and request permissions if necessary.

--- Health/src/main/java/com/sciencesquad/health/activity/MapsActivity.java
+++ Health/src/main/java/com/sciencesquad/health/activity/MapsActivity.java
@@ -20,6 +20,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.sciencesquad.health.R;
+//test change
 
 
 public class MapsActivity extends FragmentActivity implements
