fed16b79ef91b19927433e36f9e321ca82580e2d
Colin Ford <colinford@purdue.edu>
2016-04-06 01:43:09 -0400

    Tweaks
    Fixing / updating things to abide by new master ‘rules’

--- Health/src/main/java/com/sciencesquad/health/core/HostActivity.java
+++ Health/src/main/java/com/sciencesquad/health/core/HostActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.sciencesquad.health.R;
+import com.sciencesquad.health.overview.OverviewFragment;
 import com.sciencesquad.health.run.RunFragment;
 import com.sciencesquad.health.core.alarm.AlarmFragment;
 import com.sciencesquad.health.nutrition.NutritionFragment;
@@ -27,6 +28,11 @@ public class HostActivity extends AppCompatActivity implements OnNavigationItemS
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		getFragmentManager()
+				.beginTransaction()
+				.replace(R.id.content, new OverviewFragment(), OverviewFragment.TAG)
+				.addToBackStack(OverviewFragment.TAG)
+				.commit();
 		this.drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 		navigationView.setNavigationItemSelectedListener(this);
@@ -43,7 +49,13 @@ public class HostActivity extends AppCompatActivity implements OnNavigationItemS
 	@Override
 	public boolean onNavigationItemSelected(MenuItem item) {
 		int id = item.getItemId();
-		if (id == R.id.nav_run) {
+		if (id == R.id.nav_overview) {
+			getFragmentManager()
+					.beginTransaction()
+					.replace(R.id.content, new OverviewFragment(), OverviewFragment.TAG)
+					.addToBackStack(OverviewFragment.TAG)
+					.commit();
+		} else if (id == R.id.nav_run) {
 			getFragmentManager()
 					.beginTransaction()
 					.replace(R.id.content, new RunLandingFragment(), RunLandingFragment.TAG)
--- Health/src/main/java/com/sciencesquad/health/overview/OverviewFragment.java
+++ Health/src/main/java/com/sciencesquad/health/overview/OverviewFragment.java
@@ -8,32 +8,56 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.transition.Visibility;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sciencesquad.health.R;
+import com.sciencesquad.health.core.BaseFragment;
+import com.sciencesquad.health.core.Module;
 import com.sciencesquad.health.core.ui.EmergencyNotification;
+import com.sciencesquad.health.core.ui.RevealTransition;
+import com.sciencesquad.health.databinding.FragmentSleepBinding;
+import com.sciencesquad.health.sleep.SleepModule;
 
-public class OverviewFragment extends Fragment {
+public class OverviewFragment extends BaseFragment {
     public static final String TAG = OverviewFragment.class.getSimpleName();
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_overview, container, false);
+    protected BaseFragment.Configuration getConfiguration() {
+        String _ = OverviewModule.TAG; // instantiates the Module...
+        return new BaseFragment.Configuration(
+                TAG, "Overview", R.drawable.ic_menu_overview,
+                R.style.AppTheme_Sleep, R.layout.fragment_overview
+        );
+    }
+
+    // This MUST change at some point. Or go away.
+    @Override @SuppressWarnings("unchecked")
+    protected FragmentSleepBinding xml() {
+        return super.xml();
+    }
+
+    @Override
+    public void onSetupTransition() {
+        this.setEnterTransition(new RevealTransition(Visibility.MODE_IN));
+        this.setExitTransition(new RevealTransition(Visibility.MODE_OUT));
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        //xml().setModule(Module.moduleForClass(OverviewModule.class));
 
+
+        // FABulous!!!
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.overviewFab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fab.setSelected(!fab.isSelected());
-                fab.setImageResource(R.drawable.animated_plus);
+                fab.setImageResource(R.drawable.ic_plus);
                 Drawable drawable = fab.getDrawable();
                 if (drawable instanceof Animatable) {
                     ((Animatable) drawable).start();
