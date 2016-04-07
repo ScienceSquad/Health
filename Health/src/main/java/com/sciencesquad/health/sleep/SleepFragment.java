272993efe7e50fb97b512fa5516a52747d14d112
Andrew Walden <waldena@purdue.edu>
2016-04-06 20:41:45 -0400

    Implemented parameter setting functions for NutrientQuery

--- Health/src/main/java/com/sciencesquad/health/sleep/SleepFragment.java
+++ Health/src/main/java/com/sciencesquad/health/sleep/SleepFragment.java
@@ -32,7 +32,6 @@ import com.sciencesquad.health.core.ui.RevealTransition;
 import com.sciencesquad.health.core.util.StaticPagerAdapter;
 import com.sciencesquad.health.core.util.X;
 import com.sciencesquad.health.databinding.FragmentSleepBinding;
-import rx.Subscription;
 
 // TODO: Preference for roommate/partner sleeping in same bed, other room, none.
 public class SleepFragment extends BaseFragment {
