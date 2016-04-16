d93781b9da83983bc6d78db5d2d785574f0cbe0f
Daniel Miller <mill1298@purdue.edu>
2016-04-16 15:46:56 -0400

    Removed Uneeded Imports.

--- Health/src/main/java/com/sciencesquad/health/nutrition/DietDialogFragment.java
+++ Health/src/main/java/com/sciencesquad/health/nutrition/DietDialogFragment.java
@@ -5,7 +5,6 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputType;
@@ -15,9 +14,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.sciencesquad.health.R;
-import com.sciencesquad.health.core.BaseApp;
-import com.sciencesquad.health.core.EventBus;
-import com.sciencesquad.health.core.EventBus.Entry;
 
 import java.util.ArrayList;
 
